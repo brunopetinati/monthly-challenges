357a6c8b0cf5309c9c4422524f4a6d70fa19ae64
Bruno Petinati <brunopetinati@hotmail.com>
2022-08-11 03:36:00 -0300

    for tag and url tag used in view. dynamic urls implemented

--- challenges/views.py
+++ challenges/views.py
@@ -22,16 +22,11 @@ monthly_challenges = {
 }
 
 def index(request):
-    list_items = ""
     months = list(monthly_challenges.keys())
 
-    for month in months:
-        capitalized_month = month.capitalize()
-        month_path = reverse('path_name', args=[month])
-        list_items += f"<li><a href='{month_path}'>{capitalized_month}</a></li>"
-
-    response_data = f"<ul>{list_items}</ul>"
-    return HttpResponse(response_data)
+    return render(request, 'challenges/index.html', {
+        'months': months # and that's how you provide a list for the template to iterate
+    })
 
 def monthly_challenge_by_number(request, month):
     months = list(monthly_challenges.keys()) # ['january','february'...]
@@ -53,7 +48,7 @@ def monthly_challenge(request, month):
 
         # always takes request as first argument
         return render(request, 'challenges/challenge.html', { # object with the variables and their values
-            'month': month.capitalize(), 
+            'month': month, 
             'challenge': challenge_text
         }) 
         # response_data = render_to_string('challenges/challenge.html')
